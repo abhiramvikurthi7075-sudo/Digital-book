d9997f26a7dc1d5956a09b589dd975e1d0ad72ad
abhiramvikurthi7075-sudo <abhiramvikurthi7075@gmail.com>
2026-04-01 17:26:21 +0530

    Initialize App component with basic structure

---
+++ app.jsx
@@ -0,0 +1,16 @@
+import { useState, } from 'react'
+import './App.css'
+import { BrowserRouter, Navigate, Routes } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // Navigate("/ViewBook")
+
+  return (
+     <div>
+     </div>
+  )
+}
+
+export default App
+
